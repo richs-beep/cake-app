d9266be04625ef39efbc1e8b315a4f07f286a8fe
richs-beep <richsulpak@gmail.com>
2026-02-13 17:53:01 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cake-app-v9.2'; // Поменял версию, чтобы обновилось
+const CACHE_NAME = 'cake-app-v9.3'; // Поменял версию, чтобы обновилось
 const ASSETS = [
   './',
   './index.html',
